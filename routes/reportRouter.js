389667a1607a99fed94673cacbb2e820dbc63da4
Thu Thảo Nguyễn <duchuy23301@gmail.com>
2022-11-18 14:02:00 +0700

    add removeBlock

--- routes/reportRouter.js
+++ routes/reportRouter.js
@@ -4,6 +4,8 @@ const ReportController = require('../controllers/reportController');
 
 Router.get('/',ReportController.getAllReports);
 Router.post('/',ReportController.addReport);
+Router.put('/:userId',ReportController.removeBlock);
+
 Router
     .route('/:reportId')
     .get(ReportController.getReportById)
